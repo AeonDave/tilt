b7e71a6043ae15e6ed5c777c65476fec2365593a
AeonDave <AeonDave@users.noreply.github.com>
2014-04-28 14:05:00 +0200

    Create logger.py

---
+++ lib/logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+"""
+Copyright (c) 2014 tilt (https://github.com/AeonDave/tilt)
+See the file 'LICENSE' for copying permission
+"""
+
+import logging, sys
+
+logger = logging.getLogger('tiltLogger')
+stream = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("\r[%(asctime)s] [%(levelname)s] %(message)s", "%H:%M:%S")
+stream.setFormatter(formatter)
+logger.addHandler(stream)
+logger.setLevel(logging.INFO)
