1af34fa1e249e87f2045f0e12f7b2d7c49680fc9
AeonDave <AeonDave@users.noreply.github.com>
2014-04-28 14:16:18 +0200

    Update update.py

--- lib/update.py
+++ lib/update.py
@@ -19,7 +19,6 @@ def update():
         msg = "[*] Updating Tilt from latest version from the GitHub Repository\n" 
         logger.info(msg)
         process = Popen("git pull origin master", shell=True, stdout=PIPE, stderr=PIPE)
-        Popen("chmod +x ./tilt.py", shell=True, stdout=PIPE, stderr=PIPE)
         process.communicate()
         success = not process.returncode
                 
