11f6b1702e28755729c7ca6b28a6bd9376e91914
AeonDave <AeonDave@users.noreply.github.com>
2014-04-28 14:05:23 +0200

    Create settings.py

---
+++ lib/settings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+"""
+Copyright (c) 2014 tilt (https://github.com/AeonDave/tilt)
+See the file 'LICENSE' for copying permission
+"""
+
+import os ,sys 
+
+VERSION = "1.1"
+AUTHOR = "AeonDave"
+DESCRIPTION = "automatic SQL injection and database takeover tool"
+SITE = "https://github.com/AeonDave/tilt.git"
+ISSUES_PAGE = ""
+GIT_REPOSITORY = "git://github.com/AeonDave/tilt.git"
+
+PLATFORM = os.name
+ROOTDIR = os.getcwd()
+PYVERSION = sys.version.split()[0]
