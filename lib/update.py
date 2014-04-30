012a49e5b59b34d61c5a434234ea31dedd7052b8
AeonDave <nova.davide@gmail.com>
2014-04-30 16:03:52 +0200

    updated update routine

--- lib/update.py
+++ lib/update.py
@@ -10,11 +10,17 @@ from lib.logger import logger
 from subprocess import PIPE
 from subprocess import Popen
 from settings import ROOTDIR
+from settings import PLATFORM
 
 def update():
     if not os.path.exists(os.path.join(ROOTDIR, ".git")):
         msg = "[-] Not a git repository. Please checkout the repository from GitHub (e.g. git clone https://github.com/AeonDave/tilt.git)"
         logger.error(msg)
+    if PLATFORM == 'nt':
+        msg = "[-] Please checkout the repository from GitHub with GitHub for Windows (e.g. https://windows.github.com)"
+        logger.warning(msg)
+        msg = "[*] Repository at https://github.com/AeonDave/tilt.git"
+        logger.info(msg)
     else:
         msg = "[*] Updating Tilt from latest version from the GitHub Repository\n" 
         logger.info(msg)
