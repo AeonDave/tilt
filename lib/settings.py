9329c266d09717e80c2a9f1146f0ca45f690d76a
AeonDave <AeonDave@users.noreply.github.com>
2014-04-30 15:35:25 +0200

    Update settings.py

--- lib/settings.py
+++ lib/settings.py
@@ -9,11 +9,12 @@ import os ,sys
 
 VERSION = "0.5.1 beta"
 AUTHOR = "AeonDave"
-DESCRIPTION = "automatic SQL injection and database takeover tool"
+DESCRIPTION = "Automatic ip lookup and reverse probing tool"
 SITE = "https://github.com/AeonDave/tilt.git"
 ISSUES_PAGE = ""
 GIT_REPOSITORY = "git://github.com/AeonDave/tilt.git"
 
 PLATFORM = os.name
-ROOTDIR = sys.path[0]
+ROOTDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+LIBDIR = os.path.abspath(os.path.dirname(__file__))
 PYVERSION = sys.version.split()[0]
